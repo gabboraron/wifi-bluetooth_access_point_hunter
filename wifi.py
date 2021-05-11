8c2693c2a116ccaf112e64578b4d501d16b21d04
gabboraron <gabboraron@gmail.com>
2021-05-12 00:23:10 +0300

    közelben levő wifik és már ismert wifik összehasonlítása

--- wifi.py
+++ wifi.py
@@ -8,6 +8,8 @@ if os.name == 'nt':
 	'''
 	Ehhez a részhez ezt avettem alapul: https://github.com/davidbombal/red-python-scripts/blob/main/windows10-wifi.py
 	'''		
+	
+	
 	commandOutput = subprocess.run(["netsh","wlan","show","profiles"], capture_output = True).stdout.decode()
 	#print(commandOutput)
 	names = (re.findall("All User Profile     : (.*)\r", commandOutput))
@@ -32,7 +34,27 @@ if os.name == 'nt':
 	print("\n\nA már ismert és mentett hálózatok:\n");				
 	for x in range(len(wifilist)):
 		print(wifilist[x])
-	print("\n\n\tKész.");
+	
 
+
+	'''
+	Ehhez a részhez ezt vettem alapul: https://www.geeksforgeeks.org/how-to-find-available-wifi-networks-using-python/
+	másik megoldás: https://stackoverflow.com/a/3191659/6274697
+	'''
+	print("\n\nJelenleg elérhető hálózatok:\n");			
+	commandOutput = subprocess.run(["netsh", "wlan", "show", "network", "mode=Bssid"], capture_output = True).stdout.decode()
+	currentAvailableNetworksNames    = (re.findall("SSID \d : (.*)\r", commandOutput))
+	#currentAvailableNetworksBSSID = (re.findall("         Signal             : (\d+)%\r", commandOutput))
+	#currentAvailableNetworksStrength = (re.findall("         Signal             : (\d+)%\r", commandOutput))
+	#print(currentAvailableNetworksNames);			
+	for x in range(len(currentAvailableNetworksNames)):
+		print(currentAvailableNetworksNames[x])
+
+	print("\n\nEgyezések:\n");			
+	for x in range(len(wifilist)):
+		if wifilist[x]["ssid"] in currentAvailableNetworksNames:
+			print(wifilist[x])
+
+	print("\n\n\tKész.");
 else:
 	print("NOT SUPPORTED");
